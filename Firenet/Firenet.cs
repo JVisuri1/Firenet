f65dee43b63f2f2c6937ac1d15c39725687bf046
JVisuri1 <34973409+JVisuri1@users.noreply.github.com>
2021-01-25 18:33:53 +0200

    Initial commit

---
+++ Firenet/Firenet.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Firenet
+{
+    public class Firenet
+    {
+
+    }
+}
---
+++ Firenet/FirenetConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firenet
+{
+    /// <summary>
+    /// Firenet configuration class
+    /// </summary>
+    public class FirenetConfig
+    {
+        /// <summary>
+        /// Firebase access token
+        /// </summary>
+        public string token { get; private set; }
+
+        /// <summary>
+        /// Firebase address
+        /// </summary>
+        public string rootPath { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_token"></param>
+        /// <param name="_path"></param>
+        public FirenetConfig(string _token, string _path)
+        {
+            token = _token;
+            rootPath = _path;
+        }
+    }
+}
