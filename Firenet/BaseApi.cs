c51d86d4ad066382ccc9cb96b48177c8f2c13c03
JVisuri1 <34973409+JVisuri1@users.noreply.github.com>
2021-02-01 18:34:38 +0200

    Create BaseApi.cs

---
+++ Firenet/BaseApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firenet
+{
+    public class BaseApi
+    {
+        private HttpClient apiClient;
+
+        public BaseApi(FirenetConfig config)
+        {
+            apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri(config.rootPath);
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        //public async Task Post()
+        //{
+
+        //}
+
+        public async Task Get(string path, object body)
+        {
+
+        }
+    }
+}
